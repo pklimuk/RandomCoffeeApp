18c5b7f0ea4976a00ff842112cd2a2eb9a66d863
Pavel Klimuk <pavelklimuk@yahoo.com>
2021-11-14 13:48:14 +0100

    Initial commit

---
+++ src/test/java/com/pklimuk/RandomCoffee/RandomCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pklimuk.RandomCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
